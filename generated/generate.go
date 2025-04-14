8c01021a6fa9ab3a8d3b442d984343f25644d248
burce <burcev-egor@mail.ru>
2025-04-14 15:30:12 +0500

    ignore ide

--- generated/generate.go
+++ generated/generate.go
@@ -4,4 +4,3 @@ package generated
 //go:generate oapi-codegen -generate gin -package openapi -o ./openapi/server.go ../api/swagger.yaml
 //go:generate oapi-codegen -generate client -package openapi -o ./openapi/client.go ../api/swagger.yaml
 //go:generate oapi-codegen -generate spec -package openapi -o ./openapi/spec.go ../api/swagger.yaml
-//go:generate protoc --proto_path=../api --go_out=./protobuf/ --go_opt=paths=source_relative --go-grpc_out=./protobuf/ --go-grpc_opt=paths=source_relative pvz.proto
